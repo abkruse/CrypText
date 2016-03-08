24dd605ee9bf4bff3f1f119d9077512e955e9395
Alex Southwell <abkruse@gmail.com>
2016-03-07 16:36:49 -0800

    Able to encrypt a message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
   'use strict';
 
   $(document).ready(function() {
-    var encryption, Create, cols, messArr;
+    var encryption, Create, cols, secArr, messArr, numArr, splitMsg, newOrdr, alphaArr, idx, encMsgArr;
 
     var myDataRef = new Firebase('https://cryptext.firebaseio.com/');
 
@@ -18,7 +18,7 @@
       Create = new Instance();
 
       Create.name = $('#name').val();
-      Create.secret = $('#secret-word').val();
+      Create.secret = $('#secret-word').val().toLowerCase();
       Create.message = $('#message').val();
 
       encryption();
@@ -26,6 +26,14 @@
 
     encryption = function() {
       cols = Create.secret.length;
+      secArr = [];
+      alphaArr = [];
+
+      for(var n = 0; n < cols; n ++){
+        secArr.push(Create.secret[n]);
+        alphaArr.push(Create.secret[n]);
+      }
+
       messArr = [];
 
       var msg = Create.message.replace(/[.,\/@#?!$%\^&\*;:{}=\-_`~()]/g,"");
@@ -36,11 +44,38 @@
         messArr.push(msg.charAt(j));
       }
 
-      if(Create.message.length % cols !== 0) {
-        for(var i = 0; i < Create.message.length % cols; i++ )
+      if (Create.message.length % cols !== 0) {
+        for(var i = 0; i <= messArr.length % cols; i++ )
           messArr.push('x');
       }
 
+      numArr = messArr.length / cols;
+
+      splitMsg = [];
+
+      for(var k = 0; k <numArr; k ++) {
+        splitMsg.push(messArr.splice(0, cols));
+      }
+
+      newOrdr = [];
+
+      alphaArr.sort();
+
+      for (var m = 0; m < secArr.length; m++ ) {
+        newOrdr.push(alphaArr.indexOf(secArr[m]));
+      }
+
+      encMsgArr = [];
+
+      for(var y = 0; y < newOrdr.length; y++) {
+        idx = newOrdr.indexOf(y);
+        for(var z = 0; z < numArr; z++) {
+          encMsgArr.push(splitMsg[z][idx]);
+        }
+      }
+
+      $('form').after('<p>' + encMsgArr.join('') + '</p>');
     };
+
   });
 })();
