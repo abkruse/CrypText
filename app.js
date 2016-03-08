97aa8eb1e8f2d39536b4ebdc056df69529204a0d
Alex Southwell <abkruse@gmail.com>
2016-03-07 20:38:07 -0800

    Able to unencode a message

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
   'use strict';
 
   $(document).ready(function() {
-    var encryption, Create, cols, secArr, messArr, numArr, splitMsg, newOrdr, alphaArr, idx, encMsgArr;
+
+    var encryption, Create, cols, secArr, messArr, numArr, splitMsg, newOrdr, alphaArr, idx, encMsgArr, getURL, decode, msg;
 
     var myDataRef = new Firebase('https://cryptext.firebaseio.com/');
 
@@ -28,15 +29,14 @@
       cols = Create.secret.length;
       secArr = [];
       alphaArr = [];
+      messArr = [];
 
       for(var n = 0; n < cols; n ++){
         secArr.push(Create.secret[n]);
         alphaArr.push(Create.secret[n]);
       }
 
-      messArr = [];
-
-      var msg = Create.message.replace(/[.,\/@#?!$%\^&\*;:{}=\-_`~()]/g,"");
+      msg = Create.message.replace(/[.,\/@#?!$%\^&\*;:{}=\-_`~()]/g,"");
 
       msg = msg.replace(/\s+/g, '');
 
@@ -53,7 +53,7 @@
 
       splitMsg = [];
 
-      for(var k = 0; k <numArr; k ++) {
+      for(var k = 0; k < numArr; k ++) {
         splitMsg.push(messArr.splice(0, cols));
       }
 
@@ -74,8 +74,93 @@
         }
       }
 
-      $('form').after('<p>' + encMsgArr.join('') + '</p>');
+      var encrypted = encMsgArr.join('');
+
+      $('form').after('<p>' + encrypted + '</p>');
+
+      myDataRef.push({
+        name: Create.name,
+        secret: Create.secret,
+        message: encrypted
+      });
     };
+
+    getURL = function(){
+      //on click, produce unique URL for from firebase
+    };
+
+    $('.decoder-ring').on('click', function(e){
+      e.preventDefault();
+
+      var secret = $('#secret-word').val();
+      var message = "nalcxehwttdttfseeleedsoaxfeahl";
+      var returnMsg = [];
+
+      for (var y = 0; y < message.length; y ++) {
+        returnMsg.push(message.charAt(y));
+      }
+
+      var decodePass = [];
+      var alphaPass = [];
+
+      for(var i = 0; i < secret.length; i ++) {
+        decodePass.push(secret.charAt(i));
+        alphaPass.push(secret.charAt(i));
+      }
+
+      alphaPass.sort();
+
+      var jmbleMsg = [];
+
+      numArr = message.length / secret.length;
+
+      for (var l = 0; l < secret.length; l++) {
+        for (var j = l; j < message.length; j += numArr) {
+          jmbleMsg.push(returnMsg[j]);
+        }
+      }
+
+      splitMsg = [];
+
+      for(var p = 0; p < numArr; p++) {
+        splitMsg.push(jmbleMsg.splice(0, secret.length));
+      }
+
+      newOrdr = [];
+
+      for (var m = 0; m < decodePass.length; m++ ) {
+        newOrdr.push(decodePass.indexOf(alphaPass[m]));
+      }
+
+      var mixMsg = [];
+      var roteMsg = [];
+
+      for (var z = 0; z < newOrdr.length; z++) {
+        idx = newOrdr.indexOf(z);
+
+        for(var x = 0; x < numArr; x++) {
+          mixMsg.push(splitMsg[x][idx]);
+        }
+        roteMsg.push(mixMsg.splice(0, numArr));
+      }
+      var decodedArr = [];
+
+      for(var a = 0; a < numArr; a++ ) {
+        for (var b = 0; b < secret.length; b++) {
+          decodedArr.push(roteMsg[b][a]);
+        }
+      }
+      console.log(decodedArr.join(''));
+    });
+
+    // decode = function(){
+
+      //sort the keyword
+
+      //split message into arrays equal in length to the keyword
+
+
+    // };
 
   });
 })();
